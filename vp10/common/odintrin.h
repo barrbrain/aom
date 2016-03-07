dd60d9223513119ac62df99b96bb698d3113013a
Nathan E. Egge <negge@dgql.org>
2016-03-06 21:23:54 -0500

    Use the Daala implementation of OD_ILOG().
    The Daala implementation of OD_ILOG(x) computes the number of bits
     necessary to represent the unsigned integer x in two's compliment.
    
    Change-Id: Ib75c2f47dca7027b3cfc40b110f2d493f9690c9c

--- vp10/common/odintrin.h
+++ vp10/common/odintrin.h
@@ -24,11 +24,57 @@ typedef int16_t dering_in;
 #define OD_MINI VPXMIN
 #define OD_CLAMPI(min, val, max) clamp((val), (min), (max))
 
-#  define OD_ILOG_NZ(x) get_msb(x)
+/*Enable special features for gcc and compatible compilers.*/
+# if defined(__GNUC__) && defined(__GNUC_MINOR__) && defined(__GNUC_PATCHLEVEL__)
+#  define OD_GNUC_PREREQ(maj, min, pat)                             \
+  ((__GNUC__ << 16) + (__GNUC_MINOR__ << 8) + __GNUC_PATCHLEVEL__ >= ((maj) << 16) + ((min) << 8) + pat)
+# else
+#  define OD_GNUC_PREREQ(maj, min, pat) (0)
+# endif
+
+int od_ilog(uint32_t _v);
+
+/*Count leading zeros.
+  This macro should only be used for implementing od_ilog(), if it is defined.
+  All other code should use OD_ILOG() instead.*/
+# if defined(_MSC_VER)
+#  include <intrin.h>
+#  if !defined(snprintf)
+#   define snprintf _snprintf
+#  endif
+/*In _DEBUG mode this is not an intrinsic by default.*/
+#  pragma intrinsic(_BitScanReverse)
+
+static __inline int od_bsr(unsigned long x) {
+  unsigned long ret;
+  _BitScanReverse(&ret, x);
+  return (int)ret;
+}
+#  define OD_CLZ0 (1)
+#  define OD_CLZ(x) (-od_bsr(x))
+# elif defined(ENABLE_TI_DSPLIB)
+#  include "dsplib.h"
+#  define OD_CLZ0 (31)
+#  define OD_CLZ(x) (_lnorm(x))
+# elif OD_GNUC_PREREQ(3, 4, 0)
+#  if INT_MAX >= 2147483647
+#   define OD_CLZ0 ((int)sizeof(unsigned)*CHAR_BIT)
+#   define OD_CLZ(x) (__builtin_clz(x))
+#  elif LONG_MAX >= 2147483647L
+#   define OD_CLZ0 ((int)sizeof(unsigned long)*CHAR_BIT)
+#   define OD_CLZ(x) (__builtin_clzl(x))
+#  endif
+# endif
+# if defined(OD_CLZ)
+#  define OD_ILOG_NZ(x) (OD_CLZ0 - OD_CLZ(x))
 /*Note that __builtin_clz is not defined when x == 0, according to the gcc
- *    documentation (and that of the x86 BSR instruction that implements it), so
- *       we have to special-case it.
- *         We define a special version of the macro to use when x can be zero.*/
-#  define OD_ILOG(x) ((x) ? OD_ILOG_NZ(x) : 0)
+   documentation (and that of the x86 BSR instruction that implements it), so
+   we have to special-case it.
+  We define a special version of the macro to use when x can be zero.*/
+#  define OD_ILOG(x) (OD_ILOG_NZ(x) & -!!(x))
+# else
+#  define OD_ILOG_NZ(x) (od_ilog(x))
+#  define OD_ILOG(x) (od_ilog(x))
+# endif
 
 #endif
