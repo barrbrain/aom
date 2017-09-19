0f5fb5dbfb774935f319dd91c398b5e59a9155ae
Nathan E. Egge <negge@mozilla.com>
2017-09-19 09:43:47 -0400

    Allow IDTX with 32x32 blocks.
    Change-Id: Iddb97f7af24afca625a2af4a3307ad683b6a906f

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -1374,8 +1374,7 @@ static INLINE TX_TYPE av1_get_tx_type(PLANE_TYPE plane_type,
     return DCT_DCT;
   }
 #endif  // CONFIG_MRC_TX
-  if (xd->lossless[mbmi->segment_id] || txsize_sqr_map[tx_size] > TX_32X32 ||
-      (txsize_sqr_map[tx_size] >= TX_32X32 && !is_inter_block(mbmi)))
+  if (xd->lossless[mbmi->segment_id] || txsize_sqr_map[tx_size] > TX_32X32)
     return DCT_DCT;
   if (mbmi->sb_type >= BLOCK_8X8 || CONFIG_CB4X4) {
     if (plane_type == PLANE_TYPE_Y) {
