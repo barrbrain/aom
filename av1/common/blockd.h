62e7bde087d84e31842a2817ba4c63214a92ab8f
Nathan E. Egge <negge@mozilla.com>
2017-09-19 07:59:07 -0400

    Enable more transforms at larger sizes.
    This patch relaxes the constraint that all 32-point transforms be either
     IDXT or DCT.
    For intra frames this means that 8x32, 32x8, 16x32, 32x16 and 32x32 can
     use DCT or IDXT plus now DST on the 32-point side.
    For inter frames this means that 8x32, 32x8, 16x32, 32x16 and 32x32 can
     use DCT or IDXT plus now DST or flip DST on the 32-point side.
    
    Change-Id: I6b2f94f8282a8b83a9e1cdcededeceb0b370c045

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -989,10 +989,10 @@ static INLINE TxSetType get_ext_tx_set_type(TX_SIZE tx_size, BLOCK_SIZE bs,
 #endif  // CONFIG_MRC_TX
   if (tx_size_sqr_up == TX_32X32) return EXT_TX_SET_DCT_IDTX;
   if (is_inter)
-    return (tx_size_sqr == TX_16X16 ? EXT_TX_SET_DTT9_IDTX_1DDCT
+    return (tx_size_sqr >= TX_16X16 ? EXT_TX_SET_DTT9_IDTX_1DDCT
                                     : EXT_TX_SET_ALL16);
   else
-    return (tx_size_sqr == TX_16X16 ? EXT_TX_SET_DTT4_IDTX
+    return (tx_size_sqr >= TX_16X16 ? EXT_TX_SET_DTT4_IDTX
                                     : EXT_TX_SET_DTT4_IDTX_1DDCT);
 }
 
